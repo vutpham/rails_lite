02c70f83fa9360de0e5ab4909ec504bfa3364967
Vu Pham <vutpham@outlook.com>
2017-10-11 01:02:45 -0700

    Add route and router

--- lib/controller_base.rb
+++ lib/controller_base.rb
@@ -7,10 +7,11 @@ class ControllerBase
   attr_reader :req, :res, :params
 
   # Setup the controller
-  def initialize(req, res)
+  def initialize(req, res, params = {})
     @req = req
     @res = res
     @already_built_response = false
+    @params = params.merge(req.params)
   end
 
   # Helper method to alias @already_built_response
@@ -58,5 +59,7 @@ class ControllerBase
 
   # use this with the router to call action_name (:index, :show, :create...)
   def invoke_action(name)
+    self.send(name)
+    render(name) unless already_built_response?
   end
 end
